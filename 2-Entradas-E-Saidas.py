bdac935584a2894accc77b1b6a88b64c9f8077fa
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2021-09-08 18:37:58 +0000

    Update 2-Entradas-E-Saidas.py

--- 2-Entradas-E-Saidas.py
+++ 2-Entradas-E-Saidas.py
@@ -1,1 +1,13 @@
-
+'''
+    Em Portugol:
+    
+    Leia A,B
+    C <- (A+B)/2
+    Escreva C
+    
+    Como fica em Python....
+'''
+A = input("Digite um valor para A")
+B = input("Digite um valor para B")
+C = (A+B)/2
+print C
