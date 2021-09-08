c45e4c82381bb02f6f4bc2eec42a8a34379bd29d
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2021-09-08 22:02:09 +0000

    Update 2-Desvios-Condicionais.py

--- 2-Desvios-Condicionais.py
+++ 2-Desvios-Condicionais.py
@@ -1,1 +1,21 @@
-
+'''
+    Sintaxe do "Se"
+    Em Portugol:
+    
+    Se < condição >
+         então
+             <instruções para o caso verdade>
+         senão
+            <intruções para o caso falso>
+   Fim-Se
+   
+   
+   Em Python:
+   if <condição>:
+          <bloco de instruções para o caso de verdade>
+   else:
+          <bloco de instruções para o caso falso>
+   
+   
+   
+'''
