6408111d1e71690d223e76b81fb782910fca7dcd
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2021-09-08 19:06:52 +0000

    Update 2-Entradas-E-Saidas.py

--- 2-Entradas-E-Saidas.py
+++ 2-Entradas-E-Saidas.py
@@ -11,3 +11,44 @@ A = input("Digite um valor para A")
 B = input("Digite um valor para B")
 C = (A+B)/2
 print C
+
+
+
+'''
+    Calcular os três primeiros múltiplos de um dado número
+'''
+nummero = int(input(""))
+print(numero * 1 )
+print(numero * 2 )
+print(numero * 3 )
+
+
+
+'''
+     Calcular as raízes de uma equação de segundo grau (Que tenha raízes reais)
+'''
+
+
+
+
+'''
+    Calcular a área de um círculo, 
+''''
+   raio = input (" Digite o valor do raio ")
+   area = 3.1415926  * raio * raio
+   print("Area Do Circulo é ".area)
+    
+    
+'''    
+    retângulo, 
+'''  
+    
+    
+'''    
+    triângulo,etc
+''' 
+
+
+'''
+    Calcular o valor líquido de um salário dado, dadas também as alíquotas dos impostos sobre o valor bruto.
+'''
