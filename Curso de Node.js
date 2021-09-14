cab30731d51cbd227daea1c9061720abcbb83562
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2021-09-14 01:35:57 +0000

    Update Curso de Node.js

--- Curso de Node.js
+++ Curso de Node.js
@@ -24,3 +24,12 @@ module.exports = div;
 
 
 
+var SomaFunc = require("./somar");
+var SubFunc = require("./sub");
+var MultiFunc = require("./multi");
+var DivFunc = require("./div");
+
+console.log(SomaFunc(1,2));
+console.log(SubFunc(10,5));
+console.log(MultiFunc(100,10));
+console.log(DivFunc( ));
