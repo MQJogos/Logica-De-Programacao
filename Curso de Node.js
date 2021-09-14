76922d28df80db5199def1670adbe6c7f0740daf
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2021-09-14 01:16:11 +0000

    Create Curso de Node.js

---
+++ Curso de Node.js
@@ -0,0 +1,26 @@
+var soma = function(a,b){
+  return a+b;
+}
+module.exports = soma;
+
+
+var sub = function(a,b){
+  return a-b;
+}
+module.exports = sub;
+
+
+var multi = function(a,b){
+  return a*b;
+}
+modue.exports = multi;
+
+
+var div = function(a,b){
+  return a/b;
+}
+module.exports = div;
+
+
+
+
