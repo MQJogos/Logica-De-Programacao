f96ba89d6de52edd683f62f6cc2de6cae52d23bc
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2021-12-27 12:33:58 -0300

    Update AlgoritmosExemplos.js

--- Visualg/AlgoritmosExemplos.js
+++ Visualg/AlgoritmosExemplos.js
@@ -1,3 +1,29 @@
+algoritmo "Menorde3"
+
+var Menor, V1 : real
+inicio
+
+escreval( "Este programa calcula o menor de três números digitados.")
+escreva("Qual o primeiro valor ? ")
+leia(Menor)
+escreva("Qual o segundo valor ? ")
+leia(V1)
+se V1 < Menor entao
+   Menor <- V1
+fimse
+escreva("Qual o terceiro valor ? ")
+leia(V1)
+se V1 < Menor entao
+   Menor <- V1
+fimse
+escreval( "O menor valor digitado foi : " , Menor)
+fimalgoritmo
+
+
+
+
+
+
 algoritmo "MEDIA ARITMETICA"
 // Seção de Declarações
 var
