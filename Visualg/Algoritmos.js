fc4319733fcde41e16404a912e85874001d53f49
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2022-01-07 21:30:31 -0300

    Update Algoritmos.js

--- Visualg/Algoritmos.js
+++ Visualg/Algoritmos.js
@@ -1,5 +1,77 @@
-VÍDEO AULA 11
+VÍDEO AULA 11 "PRÁTICA - Estruturas de repetição 3"
+Atividade 01
+Desenvolva um programa usando a estrutura “para” que mostre na tela a
+seguinte contagem: 
+     0 5 10 15 20 25 30 35 40 Acabou!
 
+
+
+
+
+
+
+
+
+
+
+
+
+Atividade 02
+Desenvolva um programa usando a estrutura “para” que mostre na tela a
+seguinte contagem:
+     100 90 80 70 60 50 40 30 20 10 0 Acabou!
+
+
+
+
+
+
+
+
+
+
+
+Atividade 03
+Escreva um programa que leia um número qualquer e mostre a tabuada desse
+número, usando a estrutura “para”.
+Ex: Digite um valor: 5
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15 ...
+
+
+
+
+
+
+
+
+
+
+
+Atividade 04
+Faça um programa usando a estrutura “para” que leia um número inteiro
+positivo e mostre na tela uma contagem de 0 até o valor digitado: 
+Ex: Digite um valor: 9
+Contagem: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, FIM!
+
+
+
+
+
+
+
+
+
+
+
+Atividade 05
+Crie um programa que leia sexo e peso de 8 pessoas, usando a estrutura
+“para”. No final, mostre na tela: 
+Quantas mulheres foram cadastradas 
+Quantos homens pesam mais de 100Kg 
+A média de peso entre as mulheres 
+O maior peso entre os homens
 
 
 
