6d14355ecc01d134708ab98e1339695c28a2a47c
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2022-01-19 16:51:34 -0300

    Update Algoritmos.js

--- Visualg/Algoritmos.js
+++ Visualg/Algoritmos.js
@@ -72,10 +72,13 @@ Algoritmo "Vetores05"
 //  Crie um programa que preencha automaticamente (usando lógica, não apenas
 //atribuindo diretamente) um vetor numérico com 15 posições com os primeiros
 //elementos da sequência de Fibonacci:
-
-
+    
+Var
+   Valor:Vetor[1..15] De Inteiro
+   Contador: Inteiro
+ 
 Inicio
-// Seção de Comandos
+    Para Contador <- 1 Ate 15 Faca 
 FimAlgoritmo
 
 
