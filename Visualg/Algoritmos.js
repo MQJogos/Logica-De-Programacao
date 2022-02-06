9a832b32fe28def7e330e6e946b95dd24bb8f800
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2022-02-05 21:02:17 -0300

    Update Algoritmos.js

--- Visualg/Algoritmos.js
+++ Visualg/Algoritmos.js
@@ -1,12 +1,6 @@
 //Requisitos: 
-//1 - O usuário deve colocar quantos grupos vão ter no campeonato.
-//2 - O usuário deve colocar quantas vezes por grupo.
-//3 - O usuário deve escolher se quer colocar o nome dos tempos ou ser nomes classificados. (Caso o usuário queira nomes sorteados criar uma lista pré definida e usar a mesma para definir os nomes)
-//4 - Para fase de grupos todos os placas dos jogos serão sorteados(Pesquisar como gerar um número aleatório) no caso de vitória somar 3 pontos, empate 1, derrota 0 ponto
-//5 - Os 2 melhores tempos de cada grupo vão passar para os Playoffs
-//6 - Nos playoffs quem ganha a fase, Caso tenha empate o jogo vai para os pênaltis que será sorteado similar ao placar do jogo. Obs: 5 pênaltis para lado e caso tenha empate novamente será pênaltis alternados quem fizer e outro errar o tempo ganha.
-//7 - Playoff vai até os termos um Campeão.
-//8 - Imprimir dos resultados inclusive pênaltis
+
+
 
 Var
    Times: Vetor [1..8] De Caracter
@@ -14,21 +8,31 @@ Var
    Cont, Soma, Soma, Jogos : Inteiro
    
 Inicio
-
+      
+      //1 - O usuário deve colocar quantos grupos vão ter no campeonato.
       Para Cont De 1 Ate 6 Faca 
       Escreva("DIGITE O NOME DO ", Cont, "° TIME: ")
       Leia(Times[Cont])
       FimPara
-      
+      //2 - O usuário deve colocar quantas vezes por grupo.
       Para Cont De 1 Ate 6 Faca 
       Escreva("DIGITE O NÚMERO DE JOGOS DO ", Cont, "° TIME: ")
       Leia(Jogos[Cont])
       FimPara
-      
+      //3 - O usuário deve escolher se quer colocar o nome dos tempos ou ser nomes classificados.
+      //(Caso o usuário queira nomes sorteados criar uma lista pré definida e usar a mesma para definir os nomes)
       Para Cont De 1 Ate 6 Faca 
       Escreva("DIGITE O NÚMERO DE VITÓRIAS DO ",Cont,"° TIME: ")
       Leia(Vitoria[Cont])
       FimPara
+      
+      
+     //4 - Para fase de grupos todos os placas dos jogos serão sorteados(Pesquisar como gerar um número aleatório) no caso de vitória somar 3 pontos, empate 1, derrota 0 ponto
+     //5 - Os 2 melhores tempos de cada grupo vão passar para os Playoffs
+     //6 - Nos playoffs quem ganha a fase, Caso tenha empate o jogo vai para os pênaltis que será sorteado similar ao placar do jogo. Obs: 5 pênaltis para lado e caso tenha empate novamente será pênaltis alternados quem fizer e outro errar o tempo ganha.
+     //7 - Playoff vai até os termos um Campeão.
+     //8 - Imprimir dos resultados inclusive pênaltis
+      
       
       Para Cont De 1 Ate 6 Faca 
       Escreva("DIGITE O NÚMERO DE EMPATES DO ",Cont, "° TIME: ")
